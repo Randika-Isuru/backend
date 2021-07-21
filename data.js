a8a50a40f18aa7b2a7188dc9a28c0fb8bb1e2450
Randika-Isuru <randikaisuru@gmail.com>
2021-07-22 01:42:14 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,29 @@
+let data = {
+    products:[
+        {
+            name:'Nike Slim Shirt',
+            code:'Nike123',
+            price:120,
+            countInStock : 10,
+            brand:'Nike'
+        },
+        {
+            name:'Adidas Fit Shirt',
+            code:'Adidas123',
+            price:100,
+            countInStock : 20,
+            brand:'Nike',
+        },
+        {
+            name:'Lacoste Free Shirt',
+            code:'Lacoste123',
+            price:220,
+            countInStock : 0,
+            brand:'Nike',
+        }
+    ]
+}
+
+export default data
+
+
